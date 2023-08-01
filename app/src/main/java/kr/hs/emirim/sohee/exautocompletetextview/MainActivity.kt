bfb6cd7fcb604207b4f2ec295a21207cee3e74d2
ksh25 <ksh251024@naver.com>
2023-08-01 20:08:23 +0900

    자동완성 텍스트뷰

---
+++ app/src/main/java/kr/hs/emirim/sohee/exautocompletetextview/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.emirim.sohee.exautocompletetextview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var items = arrayOf("CSI-뉴욕","CSI-라스베가스","CSI-마이애미","Friends","Fringe","Lost","Lost World")
+    }
+}
